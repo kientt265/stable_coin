dcef7525cf1af3e7d6f2e9b9d4d12e6c058fffd1
kientt265 <kientran26503@gmail.com>
2025-04-30 01:31:10 +0700

    complete-logic-collateral-function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import {DecentralizedStableCoin} from "./DecentralizedStableCoint.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IREC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /**
-*@tiltle DSCEngine
-@author PoloStone
+ * @tiltle DSCEngine
+ * @author PoloStone
  */
 
 // Layout of Contract:
@@ -27,64 +30,101 @@ import {DecentralizedStableCoin} from "./DecentralizedStableCoint.sol";
 // - private
 // - view & pure functions
 
-contract DSCEngine{
+contract DSCEngine is ReentrancyGuard {
     /////////////////////
     ///// Errors ///////
     ///////////////////
+    error DSCEngine_NotAllowedToken();
     error DSCEngine_NeedsMoreThanZero();
     error DSCEngine_TokenAdressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine_TransferFailed();
     /////////////////////////////
     ///// State Variables ///////
     ////////////////////////////
-    mapping(address token => address priceFeed) private s_priceFeed; //tokenToPriceFeed
-    DecentralizedStableCoin private immutable i_dsc;
 
+    mapping(address token => address priceFeed) private s_priceFeed; //tokenToPriceFeed
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+    mapping(address user => uint256 admountDscMinted) private s_DSCMinted;
+    DecentralizedStableCoin private immutable i_dsc;
+    /////////////////////
+    ///// Events ///////
+    ///////////////////
+    event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
     /////////////////////
     ///// Modifier ///////
     ///////////////////
 
-    modifier moreThanZero(uint256 _amount){
-        if(_amount == 0) {
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount == 0) {
             revert DSCEngine_NeedsMoreThanZero();
         }
+        _;
     }
 
     modifier isAllowedToken(address token) {
-
+        if (s_priceFeed[token] == address(0)) {
+            revert DSCEngine_NotAllowedToken();
+        }
+        _;
     }
     /////////////////////
     ///// Functions ///////
     ///////////////////
-    constructor(
-        address[] memory tokenAdresses,
-        address[] memory priceFeedAddresses,
-        address dscAdress) {
-            if(tokenAdresses.length != priceFeedAddresses.length){
-                revert DSCEngine_TokenAdressesAndPriceFeedAddressesMustBeSameLength();
-            }
-            for (uint256 i = 0; i <tokenAdresses.length; i++){
-                s_priceFeed[tokenAdresses[i]] = priceFeedAddresses[i];
-            }
+
+    constructor(address[] memory tokenAdresses, address[] memory priceFeedAddresses, address dscAdress) {
+        if (tokenAdresses.length != priceFeedAddresses.length) {
+            revert DSCEngine_TokenAdressesAndPriceFeedAddressesMustBeSameLength();
+        }
+        for (uint256 i = 0; i < tokenAdresses.length; i++) {
+            s_priceFeed[tokenAdresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAdress);
     }
     /////////////////////
     ///// External ///////
     ///////////////////
-    function depositCollateralAndMintDsc() external{}
 
-    function depositCollareal(
-        address tokenCollateral,
-        uint256 amountCollateral
-        ) external moreThanZero(amountCollateral) {}
+    function depositCollateralAndMintDsc() external {}
 
-    function redeemCollateralForDsc() external{}
+    function depositCollareal(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAdress, amountCollateral);
+        bool susscess = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if(!susscess) {
+            revert DSCEngine_TransferFailed();
+        }
+    }
 
-    function redeemCollateral() external{}
+    function redeemCollateralForDsc() external {}
 
-    function mintDsa() external{}
+    function redeemCollateral() external {}
+
+    function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
+        s_DSCMinted[msg.sender] += amountDScToMint;
+        revertIfHealthFactorIsBroken(msg.sender);
+
+    }
 
     function burnDsc() external {}
 
     function liquidate() external {}
 
     function getHealthFactor() external view {}
+
+    /////////////////////////////////
+    ///// Private & Internal ///////
+    ///////////////////////////////
+
+    function _healthFactor(address user) private view returns(uint256){
+        
+    }
+    function revertIfHealthFactorIsBroken(adress user) internal view {
+
+    }
+
 }
--- src/DecentralizedStableCoint.sol
+++ src/DecentralizedStableCoint.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
 
 /// @title DecentralizedStableCoin
 /// @author Michael
 /// @notice Exogenous (ETH)
 //This is cthe contract meant to be governed by DSCEngine. This con tract is  just
 //the ERC 20 implementaion of our stabblecoin system
-
 
 // Layout of Contract:
 // - version
@@ -48,10 +47,10 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
-        if(_amount <= 0 ){
+        if (_amount <= 0) {
             revert DecentralizedStableCoin_MustBeMoreThanZero();
         }
-        if(balance <_amount) {
+        if (balance < _amount) {
             revert DecentralizedStableCoin_BurnAmountExceedsBalance();
         }
         super.burn(_amount);
@@ -59,16 +58,15 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     // 6. External functions
 
     // 7. Public functions
-    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
-        if(_to == address(0)){
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert DecentralizedStableCoin_NotZeroAddress();
         }
-        if(_amount <= 0) {
+        if (_amount <= 0) {
             revert DecentralizedStableCoin_MustBeMoreThanZero();
         }
         _mint(_to, _amount);
         return true;
-
     }
     // 8. Internal functions
 
